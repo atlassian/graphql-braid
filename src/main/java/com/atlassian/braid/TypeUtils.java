cb1eb6503d42364b746f1d997763915b875e510a
Samuel Le Berrigaud <sam@leberrigaud.org>
2018-02-08 13:27:31 +0100

    Clean up TypeUtils#findQueryType

--- src/main/java/com/atlassian/braid/TypeUtils.java
+++ src/main/java/com/atlassian/braid/TypeUtils.java
@@ -6,6 +6,8 @@ import graphql.language.SchemaDefinition;
 import graphql.schema.idl.TypeDefinitionRegistry;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static com.atlassian.braid.SchemaBraid.QUERY_FIELD_NAME;
 import static java.util.Arrays.asList;
@@ -16,24 +18,32 @@ import static java.util.Arrays.asList;
 public class TypeUtils {
     /**
      * Finds the query type definition.
+     *
      * @param registry all types
      * @return the query type
      * @throws IllegalArgumentException if the query definition can't be found or there is no schema
      */
 
     static ObjectTypeDefinition findQueryType(TypeDefinitionRegistry registry) throws IllegalArgumentException {
-        SchemaDefinition dsSchema = registry.schemaDefinition().orElseThrow(IllegalArgumentException::new);
-        OperationTypeDefinition dsQueryType = dsSchema.getOperationTypeDefinitions().stream()
-                .filter(d -> d.getName().equals(QUERY_FIELD_NAME))
-                .findFirst()
+        return registry.schemaDefinition()
+                .map(SchemaDefinition::getOperationTypeDefinitions)
+                .flatMap(TypeUtils::findQueryOperationDefinition)
+                .map(OperationTypeDefinition::getType)
+                .flatMap(registry::getType)
+                .map(ObjectTypeDefinition.class::cast)
                 .orElseThrow(IllegalArgumentException::new);
-        return (ObjectTypeDefinition) registry.getType(dsQueryType.getType())
-                .orElseThrow(IllegalStateException::new);
+    }
+
+    private static Optional<OperationTypeDefinition> findQueryOperationDefinition(List<OperationTypeDefinition> definitions) {
+        return definitions.stream()
+                .filter(d -> Objects.equals(d.getName(), QUERY_FIELD_NAME))
+                .findFirst();
     }
 
     /**
      * Filters the top level fields on a query just to the provided list.  If no fields specified, no action is taken.
-     * @param registry the types
+     *
+     * @param registry       the types
      * @param topLevelFields the fields to allow or if empty, all of them
      */
     public static void filterQueryType(TypeDefinitionRegistry registry, String... topLevelFields) {
