976e7fcc16da71fb4807b97c3e3de154bb9da6c7
Samuel Le Berrigaud <sam@leberrigaud.org>
2018-03-29 09:35:15 +0200

    CONFDEV-58909: Add experimental document mapper
    - Bump major version number
    - add javadocs on exception

--- src/main/java/com/atlassian/braid/document/DocumentMapperException.java
+++ src/main/java/com/atlassian/braid/document/DocumentMapperException.java
@@ -2,6 +2,9 @@ package com.atlassian.braid.document;
 
 import static java.lang.String.format;
 
+/**
+ * Exception thrown when a document mapping error happens
+ */
 @SuppressWarnings("WeakerAccess")
 public final class DocumentMapperException extends RuntimeException {
     DocumentMapperException(String msg, Object... args) {
