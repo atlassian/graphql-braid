5d0f6285769d9e8564b97be7333bf5ca9b480fc2
Andreas Marek <amarek@atlassian.com>
2019-01-08 15:10:44 +1100

    remove all sources and add link to the real repo

--- src/main/java/com/atlassian/braid/java/util/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package of useful <em>small</em> extensions to the java.util package
- * All classes in this package are considered internal classes and <em>not</em> part of the Braid API
- */
-package com.atlassian.braid.java.util;
--- src/test/groovy/com/atlassian/braid/source/yaml/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Classes to help building {@link com.atlassian.braid.SchemaSource} implementations from YAML configuration.
- */
-package com.atlassian.braid.source.yaml;
